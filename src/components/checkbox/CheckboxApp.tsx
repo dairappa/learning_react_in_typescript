1b4a7f5dcfc3e327f32cc7fd8a9e9dd0fd4935c8
dairappa <dairappa@gmail.com>
2020-08-27 20:26:40 +0900

    useState を useReducerに変更

--- src/components/checkbox/CheckboxApp.tsx
+++ src/components/checkbox/CheckboxApp.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useReducer} from "react";
 
 export const CheckboxApp: React.FC = () => {
-    const [checked, setChecked] = useState(false)
+    const [checked, toggle] = useReducer(checked => !checked, false)
 
     useEffect(() => {
         console.log(`checked: ${checked.toString()}`)
@@ -12,7 +12,7 @@ export const CheckboxApp: React.FC = () => {
             <input
                 type="checkbox"
                 value={`${checked}`}
-                onChange={() => setChecked(checked => !checked)}/>
+                onChange={() => toggle()}/>
             {checked ? "checked" : "not checked"}
         </>
     )
