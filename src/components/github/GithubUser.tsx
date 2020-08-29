2e87369229a7c1a5dc75ba93fc17f5b563121126
dairappa <dairappa@gmail.com>
2020-08-29 09:11:43 +0900

    webstromの規約に従い修正

--- src/components/github/GithubUser.tsx
+++ src/components/github/GithubUser.tsx
@@ -2,29 +2,63 @@ import * as React from 'react';
 import {useEffect, useState} from 'react';
 
 export const GithubUser: React.FC<{ login: string }> = ({login}) => {
-    const [data, setData] = useState()
+    const loadJSON = (key: string) => {
+        const item = localStorage.getItem(key);
+        return key && item && JSON.parse(item);
+    }
+    const saveJSON = (key: string, data: any) =>
+        key && localStorage.setItem(key, JSON.stringify(data))
+
+    const [data, setData] = useState(
+        loadJSON(`user:${login}`)
+    )
+
+
+    const [loading, setLoading] = useState(false)
+
+    const [error, setError] = useState()
+
+    useEffect(() => {
+        if (!data) return
+
+        if (data.login === login) return
+
+        const {name, avatar_url, location} = data
+        saveJSON(`user:${login}`, {
+            name,
+            login,
+            avatar_url,
+            location
+        })
+    }, [data, login])
 
     useEffect(() => {
         (async () => {
-            if (!login) {
-                return
-            }
+            if (!login) return
+
+            if (data && data.id && data.login === login) return
+
             try {
+                setLoading(true)
                 const response = await fetch(`https://api.github.com/users/${login}`);
                 const data = await response.json();
+                setLoading(false)
                 setData(data)
             } catch (e) {
-                console.error()
+                setError(e)
             }
 
         })()
-    }, [login])
+    }, [data, login])
 
-    if (data) {
-        return (
+    if (error) return <pre>{JSON.stringify(error)}</pre>
+
+    if (!data) return null
+
+    return (
+        <>
             <pre>{JSON.stringify(data, null, 2)}</pre>
-        )
-    }
-
-    return null
+            {loading ? <h1>loading...</h1> : null}
+        </>
+    )
 }
